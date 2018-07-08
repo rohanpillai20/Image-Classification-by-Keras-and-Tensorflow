d971252dec2b89f9f1c5868f2f94d1c7848c0300
Rohan Pillai <32511124+rohanpillai20@users.noreply.github.com>
2018-07-08 15:59:26 +0530

    Add files via upload

---
+++ Using Keras/Image Classification (Draft 6) - Testing (Multiclass).py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+import time
+
+start = time.time()
+
+img_width, img_height = 150, 150
+model_path = './models/model.h5'
+model_weights_path = './models/weights.h5'
+model = load_model(model_path)
+model.load_weights(model_weights_path)
+
+
+def predict(file):
+  x = load_img(file, target_size=(img_width,img_height))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  array = model.predict(x)
+  result = array[0]
+  #print(result)
+  answer = np.argmax(result)
+  if answer == 1:
+    print("Predicted: chair")
+  elif answer == 0:
+    print("Predicted: Motorbikes")
+  elif answer == 2:
+    print("Predicted: soccer_ball")
+
+  return answer
+
+for i, ret in enumerate(os.walk('data/alien_test')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    
+    print(ret[0] + '/' + filename)
+    result = predict(ret[0] + '/' + filename)
+    print(" ")
+
+
+end = time.time()
+dur = end-start
+
+if dur<60:
+    print("Execution Time:",dur,"seconds")
+elif dur>60 and dur<3600:
+    dur=dur/60
+    print("Execution Time:",dur,"minutes")
+else:
+    dur=dur/(60*60)
+    print("Execution Time:",dur,"hours")
---
+++ Using Keras/Image Classification (Draft 6) - Training (Multiclass).py
@@ -0,0 +1,113 @@
+import sys
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense, Activation
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras import callbacks
+import time
+
+start = time.time()
+
+DEV = False
+argvs = sys.argv
+argc = len(argvs)
+
+if argc > 1 and (argvs[1] == "--development" or argvs[1] == "-d"):
+  DEV = True
+
+if DEV:
+  epochs = 2
+else:
+  epochs = 20
+
+train_data_path = 'data/train'
+validation_data_path = 'data/test'
+
+"""
+Parameters
+"""
+img_width, img_height = 150, 150
+batch_size = 32
+samples_per_epoch = 1000
+validation_steps = 300
+nb_filters1 = 32
+nb_filters2 = 64
+conv1_size = 3
+conv2_size = 2
+pool_size = 2
+classes_num = 3
+lr = 0.0004
+
+model = Sequential()
+model.add(Convolution2D(nb_filters1, conv1_size, conv1_size, border_mode ="same", input_shape=(img_width, img_height, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
+
+model.add(Convolution2D(nb_filters2, conv2_size, conv2_size, border_mode ="same"))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(pool_size, pool_size), dim_ordering='th'))
+
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Activation("relu"))
+model.add(Dropout(0.5))
+model.add(Dense(classes_num, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=lr),
+              metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_path,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_path,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+"""
+Tensorboard log
+"""
+log_dir = './tf-log/'
+tb_cb = callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0)
+cbks = [tb_cb]
+
+model.fit_generator(
+    train_generator,
+    samples_per_epoch=samples_per_epoch,
+    epochs=epochs,
+    validation_data=validation_generator,
+    callbacks=cbks,
+    validation_steps=validation_steps)
+
+target_dir = './models/'
+if not os.path.exists(target_dir):
+  os.mkdir(target_dir)
+model.save('./models/model.h5')
+model.save_weights('./models/weights.h5')
+
+end = time.time()
+dur = end-start
+
+if dur<60:
+    print("Execution Time:",dur,"seconds")
+elif dur>60 and dur<3600:
+    dur=dur/60
+    print("Execution Time:",dur,"minutes")
+else:
+    dur=dur/(60*60)
+    print("Execution Time:",dur,"hours")
