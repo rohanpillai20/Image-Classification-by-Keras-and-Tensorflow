ac163435be750217f3a779c127855fb5957e25f7
jesse250597 <93357111+jesse250597@users.noreply.github.com>
2021-10-28 17:29:54 -0700

    Added cross-validation for training

--- Using Tensorflow/Training.py
+++ Using Tensorflow/Training.py
@@ -1,8 +1,89 @@
-import dataset
-import tensorflow as tf
-import time
-import numpy as np
-import os
+from numpy import mean
+from numpy import isnan
+from numpy import asarray
+from numpy import polyfit
+from scipy.stats import pearsonr
+from matplotlib import pyplot
+from sklearn.datasets import make_classification
+from sklearn.model_selection import KFold
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+def get_dataset(n_samples=100):
+	X, y = make_classification(n_samples=n_samples, n_features=20, n_informative=15, n_redundant=5, random_state=1)
+	return X, y
+
+def get_models():
+	models = list()
+	models.append(LogisticRegression())
+	models.append(RidgeClassifier())
+	models.append(SGDClassifier())
+	models.append(PassiveAggressiveClassifier())
+	models.append(KNeighborsClassifier())
+	models.append(DecisionTreeClassifier())
+	models.append(ExtraTreeClassifier())
+	models.append(LinearSVC())
+	models.append(SVC())
+	models.append(GaussianNB())
+	models.append(AdaBoostClassifier())
+	models.append(BaggingClassifier())
+	models.append(RandomForestClassifier())
+	models.append(ExtraTreesClassifier())
+	models.append(GaussianProcessClassifier())
+	models.append(GradientBoostingClassifier())
+	models.append(LinearDiscriminantAnalysis())
+	models.append(QuadraticDiscriminantAnalysis())
+	return models
+
+def evaluate_model(cv, model):
+	X, y = get_dataset()
+	scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+	return mean(scores)
+
+ideal_cv = LeaveOneOut()
+cv = KFold(n_splits=10, shuffle=True, random_state=1)
+models = get_models()
+ideal_results, cv_results = list(), list()
+for model in models:
+	cv_mean = evaluate_model(cv, model)
+	ideal_mean = evaluate_model(ideal_cv, model)
+	if isnan(cv_mean) or isnan(ideal_mean):
+		continue
+	cv_results.append(cv_mean)
+	ideal_results.append(ideal_mean)
+	# summarize progress
+	print('>%s: ideal=%.3f, cv=%.3f' % (type(model).__name__, ideal_mean, cv_mean))
+corr, _ = pearsonr(cv_results, ideal_results)
+print('Correlation: %.3f' % corr)
+pyplot.scatter(cv_results, ideal_results)
+
+coeff, bias = polyfit(cv_results, ideal_results, 1)
+line = coeff * asarray(cv_results) + bias
+pyplot.plot(cv_results, line, color='r')
+
+pyplot.title('10-fold CV vs LOOCV Mean Accuracy')
+pyplot.xlabel('Mean Accuracy (10-fold CV)')
+pyplot.ylabel('Mean Accuracy (LOOCV)')
+
+pyplot.show()
 
 start = time.time()
 try:
